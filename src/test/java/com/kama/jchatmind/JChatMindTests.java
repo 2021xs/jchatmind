d1cc2970e99a2eb5c90a08844b61fa48507ccd53
xi-suo <3084568168@qq.com>
2026-08-24 18:00:08 +0800

    docs(test): align reproducible release configuration

--- src/test/java/com/kama/jchatmind/JChatMindTests.java
+++ src/test/java/com/kama/jchatmind/JChatMindTests.java
@@ -1,7 +1,9 @@
 package com.kama.jchatmind;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.location=classpath:/application-test.yaml")
+@ActiveProfiles("test")
 public class JChatMindTests {
 }
--- src/test/java/com/kama/jchatmind/JchatmindApplicationTests.java
+++ src/test/java/com/kama/jchatmind/JchatmindApplicationTests.java
@@ -2,8 +2,10 @@ package com.kama.jchatmind;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.location=classpath:/application-test.yaml")
+@ActiveProfiles("test")
 class JchatmindApplicationTests {
 
     @Test
